afe34cf766449e9b7518f539ced08d8cdebfb1db
Terra238 <terragion238@gmail.com>
2025-02-01 16:01:58 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
-console.log('Background script running');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "cartData") {
+    // Send cart data to the backend
+    fetch("http://localhost:5000/process-cart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message.data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Backend response:", data);
+      })
+      .catch(error => {
+        console.error("Error sending data to backend:", error);
+      });
+  }
+});
